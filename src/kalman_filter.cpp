8b191fc4e2923cbccc1f89cdb33195a90a9d3bc1
SharkyZg <marko.sarkanj@gmail.com>
2017-09-22 10:38:16 +0200

    CarND-Kalman-Filter_project

---
+++ src/kalman_filter.cpp
@@ -0,0 +1,100 @@
+#include "kalman_filter.h"
+#include "tools.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+KalmanFilter::KalmanFilter() {}
+
+
+KalmanFilter::~KalmanFilter() {}
+
+MatrixXd ConvertMeasToPolar(const VectorXd& x_state);
+  
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+
+}
+
+void KalmanFilter::Predict() {
+  /**
+  TODO:
+    * predict the state
+  */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Kalman Filter equations
+  */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Extended Kalman Filter equations
+  */
+    
+  VectorXd z_pred = ConvertMeasToPolar(x_);
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
+
+MatrixXd ConvertMeasToPolar(const VectorXd& x_state) {
+  
+  VectorXd z_polar;
+  z_polar = VectorXd(3);
+  
+  // Extract position and velocity from x_state
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+  
+  // Calculate polar variables
+  z_polar(0) = sqrt(pow(px, 2) + pow(py, 2));
+  z_polar(1) = atan2(py, px);
+  // Prevent division by 0
+  if (sqrt(pow(px, 2) + pow(py, 2)) < .00001){
+    z_polar(2) = (px * vx + py * vy) / 0.00001;
+  }
+  else{
+      z_polar(2) = (px * vx + py * vy) / sqrt(pow(px, 2)+pow(py, 2));
+  }
+  
+  return z_polar;
+  
+  }
